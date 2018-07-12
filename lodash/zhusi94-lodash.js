3b672fa5aab31ed7d5b0b120928dbd8c947f55cc
dazhoutianxiadiyi <six666@qq.com>
2018-07-12 15:19:12 +0800

    7/12/7

--- lodash/zhusi94-lodash.js
+++ lodash/zhusi94-lodash.js
@@ -25,6 +25,13 @@
         }
       }
       return result
-    }
+    },
 
+    difference: function(array, ...values) {
+      //Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons. 
+      //The order and references of result values are determined by the first array.
+
+
+
+    }
   }
