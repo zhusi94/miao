02336cbe670a0ad25a0a86e697e9385bf892cb63
dazhoutianxiadiyi <six666@qq.com>
2018-07-12 14:39:49 +0800

    7/12/3

--- lodash/zhusi94-lodash.js
+++ lodash/zhusi94-lodash.js
@@ -1,18 +1,17 @@
   var zhusi94 = {
-    chunk: function(array, [size = 1]) {
-
-      let result = []
-      let a = []
-      let i = 0
-      while (array.length > 0) {
-        i = 0
-        while (i < Math.min(size, array.length)) {
-          a.push(array.shift())
-          i++
-        }
-        result.push(a)
+    chunk: function(array, size) {
+      if (size == undefined) {
+        size = 1
       }
-
+      var result = []
+      var a = []
+      var i = 0
+      var counter = Math.ceil(array.length / size)
+      for (i; i < array.length; i++) {
+        result.push(array.slice(i, i + size))
+        i += size
+        i--
+      }
       return result
     }
   }
